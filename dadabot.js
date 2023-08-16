26651acaaa89ce6fa3d788a2c92e97a3e14ac22a
E. H. C. G <114739362+iodoGuduD@users.noreply.github.com>
2023-08-16 11:09:00 -0300

    Fix 1.1

--- dadabot.js
+++ dadabot.js
@@ -1,12 +1,16 @@
 let paragraph = document.querySelector('p');
 let button = document.querySelector('button');
 
-let phrases = ['Luiz é calvo', 'Yago era uma goiaba', 'Existe um cabeça de pirâmide'];
-let element = phrases[Math.floor(Math.random() * phrases.length)];
+function randomizer() {
+    let phrases = ['Luiz é calvo', 'Yago era uma goiaba', 'Existe um cabeça de pirâmide'];
+    let element = phrases[Math.floor(Math.random() * phrases.length)];
+
+    return element;
+}
 
 function buttonRandomPhrase() {
     button.addEventListener("click", () => {
-        paragraph.innerText(`${element}`);
+        paragraph.innerText(element);
         paragraph.innerHTML('invisible');
     });
 } 
